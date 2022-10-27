67644087dd5fc722c02eb1e2943b137710dc10a1
hieun33 <grace3029@gamil.com>
2022-10-28 08:52:11 +0900

    푸터수정중

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,7 +3,34 @@ export default function Footer() {
     return (
         <footer>
             <div className="inner">
-                <p>2022 EUNHYE &copy; ALL RIGHT RESERVED</p>
+                <img src={process.env.PUBLIC_URL+'/img/logo_f.jpg'} alt="푸터용로고회색" />
+
+                
+                <article>
+                    <h1>Company</h1>
+                    <ul>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">contact</a></li>
+                        <li><a href="#">Search</a></li>
+                    </ul>
+                </article>
+                <article>
+                    <h1>Account</h1>
+                    <ul>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Signup</a></li>
+                        
+                    </ul>
+                </article>
+                <article>
+                    <h1>Social</h1>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Github</a></li>
+                        <li><a href="#">Instagram</a></li>
+                    </ul>
+                </article>
+                {/* <p>2022 EUNHYE &copy; ALL RIGHT RESERVED</p> */}
             </div>
         </footer>
     )
